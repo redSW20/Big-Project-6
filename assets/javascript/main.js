357f72e88a2c87d6902c7c6163dcb80d8fc57c05
RedSW20 <kevbailey1227@gmail.com>
2019-03-12 01:03:02 -0500

    on click submit in place

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,10 +1,29 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAjnb5vwxhGm8XzF9us9btiVEuhAdQEcZ8",
-    authDomain: "train-project-15186.firebaseapp.com",
-    databaseURL: "https://train-project-15186.firebaseio.com",
-    projectId: "train-project-15186",
-    storageBucket: "train-project-15186.appspot.com",
-    messagingSenderId: "1011904132529"
-  };
-  firebase.initializeApp(config);
+ $(document).ready(function () {
+     // Initialize Firebase
+     var config = {
+       apiKey: "AIzaSyAjnb5vwxhGm8XzF9us9btiVEuhAdQEcZ8",
+       authDomain: "train-project-15186.firebaseapp.com",
+       databaseURL: "https://train-project-15186.firebaseio.com",
+       projectId: "train-project-15186",
+       storageBucket: "train-project-15186.appspot.com",
+       messagingSenderId: "1011904132529"
+     };
+     firebase.initializeApp(config);
+     var database = firebase.database();
+
+     //When Submit Button is Pressed, Push information to Firebase database
+     $(".submit").on("click", function () {
+       database.ref().push({
+         name: $('.trainName').val().trim(),
+         destination: $('.trainDestination').val().trim(),
+         time: $('.trainTime').val().trim(),
+         frequency: $('.trainFrequency').val().trim()
+       });
+     });
+
+
+
+
+
+     
+)};
